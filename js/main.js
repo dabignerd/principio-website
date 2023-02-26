c2c8613a554699c684c0bead1f31833c5c4de230
dominic.angkuw <angkuwdominic@gmail.com>
2023-02-25 23:26:02 -0800

    Add about.html and edit HTML, CSS, and JavaScript code.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-// code for navigation bar effects
+// find the current scroll position
 var prevScrollPos = window.pageYOffset;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollPos > currentScrollPos) {
+
+    // hide the navbar when scrolling down
     document.getElementById("navbar").style.top = "0";
   }
   else {
     document.getElementById("navbar").style.top = "-100px";
   }
-  prevScrollPos = currentScrollPos;
+
+  // assign a new scroll position value
+  prevScrollPos = currentScrollPos; 
 }
