4db243fc7e7d916ec8ac4aed62d1897442ee30c3
dominic.angkuw <angkuwdominic@gmail.com>
2023-05-01 17:31:30 -0700

    Add PHP. Review system might not work 100%... still working on it!

---
+++ reviews.php
@@ -0,0 +1,16 @@
+<?php 
+    // Connect to the database 
+    $db = new mysqli("aws.connect.psdb.cloud", "0zyfzvk2boag4sjwxdyj", "pscale_pw_vOfuBPnEqP6yucjxCKr79HDYTA0xS8Bcy3iJK6rvUgC", "reviews"); 
+ 
+    // Get the reviews from the database 
+    $query = "SELECT * FROM reviews ORDER BY id DESC"; 
+    $result = $db->query($query); 
+ 
+    // Display the reviews 
+    while ($row = $result->fetch_assoc()) { 
+        echo "<div class='review'>"; 
+        echo "<h3>" . $row["name"] . "</h3>"; 
+        echo "<p>" . $row["review"] . "</p>"; 
+        echo "</div>"; 
+    } 
+?> 
---
+++ submit_review.php
@@ -0,0 +1,17 @@
+<?php 
+    // Connect to the database 
+    $db = new mysqli("aws.connect.psdb.cloud", "0zyfzvk2boag4sjwxdyj", "pscale_pw_vOfuBPnEqP6yucjxCKr79HDYTA0xS8Bcy3iJK6rvUgC", "reviews"); 
+ 
+    // Get the form data 
+    $name = $db->real_escape_string($_POST["name"]); 
+    $email = $db->real_escape_string($_POST["email"]); 
+    $review = $db->real_escape_string($_POST["review"]); 
+ 
+    // Insert the review into the database 
+    $query = "INSERT INTO reviews (name, email, review) VALUES ('$name', '$email', '$review')"; 
+    $db->query($query); 
+ 
+    // Redirect the user to the review page 
+    header("Location: reviews.php"); 
+    exit()
+?> 
