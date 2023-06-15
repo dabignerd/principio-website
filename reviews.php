7d347492c4005369e7448d3cca1324e2b76fafe6
dominic.angkuw <angkuwdominic@gmail.com>
2023-06-15 11:49:19 -0700

    edit PHP

--- reviews.php
+++ reviews.php
@@ -1,16 +1,16 @@
 <?php 
-    // Connect to the database 
+    // connect to the database 
     $db = new mysqli("aws.connect.psdb.cloud", "0zyfzvk2boag4sjwxdyj", "pscale_pw_vOfuBPnEqP6yucjxCKr79HDYTA0xS8Bcy3iJK6rvUgC", "reviews"); 
  
-    // Get the reviews from the database 
+    // get the reviews from the database 
     $query = "SELECT * FROM reviews ORDER BY id DESC"; 
     $result = $db->query($query); 
  
-    // Display the reviews 
+    // display the reviews 
     while ($row = $result->fetch_assoc()) { 
         echo "<div class='review'>"; 
         echo "<h3>" . $row["name"] . "</h3>"; 
         echo "<p>" . $row["review"] . "</p>"; 
         echo "</div>"; 
     } 
-?> 
+?>
--- submit_review.php
+++ submit_review.php
@@ -1,17 +1,17 @@
 <?php 
-    // Connect to the database 
+    // connect to the database 
     $db = new mysqli("aws.connect.psdb.cloud", "0zyfzvk2boag4sjwxdyj", "pscale_pw_vOfuBPnEqP6yucjxCKr79HDYTA0xS8Bcy3iJK6rvUgC", "reviews"); 
  
-    // Get the form data 
+    // get the form data 
     $name = $db->real_escape_string($_POST["name"]); 
     $email = $db->real_escape_string($_POST["email"]); 
     $review = $db->real_escape_string($_POST["review"]); 
  
-    // Insert the review into the database 
+    // insert the review into the database 
     $query = "INSERT INTO reviews (name, email, review) VALUES ('$name', '$email', '$review')"; 
     $db->query($query); 
  
-    // Redirect the user to the review page 
+    // redirect the user to the review page 
     header("Location: reviews.php"); 
     exit()
 ?> 
