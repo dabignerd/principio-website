69c63ef426c851b85575a8a34305871e0ffa443a
Dominic Angkuw <angkuwdominic@gmail.com>
2024-03-26 14:09:38 -0700

    Reformat

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 // find the current scroll position
 var prevScrollPos = window.pageYOffset;
 
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        // hide the navbar when scrolling down
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
 
-    // hide the navbar when scrolling down
-    document.getElementById("navbar").style.top = "0";
-  }
-  else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-
-  // assign a new scroll position value
-  prevScrollPos = currentScrollPos; 
-}
+    // assign a new scroll position value
+    prevScrollPos = currentScrollPos;
+};
